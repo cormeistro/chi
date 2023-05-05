0efcd5de5f2ead52aa257faee602aee2061d8981
cormeistro <coreycollinsm@gmail.com>
2023-05-05 13:15:28 -0400

    Fixed stickyTop state

--- src/components/StickyTop.tsx
+++ src/components/StickyTop.tsx
@@ -1,21 +1,11 @@
 import NotificationBar from "./NotificationBar";
 import TopBar from "./TopBar";
-import { useState } from "react";
 
 const StickyTop = () => {
-  const [hideNotificationBar, setHideNotificationBar] = useState(false);
-
-  function handleHideNotificationBar(bool: boolean) {
-    setHideNotificationBar(bool);
-  }
-
   return (
-    <div className={`sticky-top ${hideNotificationBar ? "dark-theme" : ""}`}>
-      <NotificationBar
-        isHidden={hideNotificationBar}
-        onHide={handleHideNotificationBar}
-      />
-      <TopBar darkTheme={hideNotificationBar} />
+    <div className="sticky-top">
+      <NotificationBar />
+      <TopBar />
     </div>
   );
 };
